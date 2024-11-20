728f628d9ff6d4ccbe86c4e7514ca44d7cfa141a
IrfanMohammed-890 <imohdirfanrr2022@gmail.com>
2024-11-21 01:02:44 +1100

    Trying to use some variables

---
+++ src/Variables.java
@@ -0,0 +1,8 @@
+public class Variables {
+    public static void main(String[] args){
+        String name = "Irfan Mohammed";
+        int age = 26;
+        double height = 5.7;
+        System.out.println("My name is :" +name+ " and my age is " +age+ " and my height is " +height);
+    }
+}
